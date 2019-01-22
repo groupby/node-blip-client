dd7bf5c0412cccb3396ec4c3fcc9924bed9627b5
Henrique Salvadori Coelho <henrique.coelho@groupbyinc.com>
2019-01-22 17:03:42 -0500

    Update blip-client.d.ts

--- blip-client.d.ts
+++ blip-client.d.ts
@@ -1,14 +1,12 @@
 
-declare module 'blip-client'
-{
+declare module 'blip-client' {
   export interface IBlipClient {
-    write(data: any): void;
+    write(data: { date: string; } & Record<string, any>): Promise<any>;
   }
 
- export function createClient(
-  host: string, 
-  port: number, 
-  clientService: string, 
-  clientEnviroment: string): IBlipClient;
- 
+  export function createClient(
+    host: string, 
+    port: number, 
+    clientService: string, 
+    clientEnviroment: string): IBlipClient;
 }
